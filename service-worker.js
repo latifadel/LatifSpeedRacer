a4a2781339e92120f6a323aa3ebd913b56bd090f
L17 <a.alabdullatif.17@gmail.com>
2025-02-09 00:40:55 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,56 +1,43 @@
 /*******************************************
  * service-worker.js
  * 
- * This file handles offline caching of
- * assets for the Speed Racer Game.
+ * Basic offline caching.
  *******************************************/
 
-// Cache version (change to force update)
-const CACHE_NAME = "speed-racer-cache-v1";
-
-// List of files to cache
+const CACHE_NAME = "speed-racer-extended-v1";
 const CACHE_ASSETS = [
-  "/",                // might need adjusting depending on your hosting
+  "/",
   "index.html",
   "style.css",
   "script.js",
   "manifest.json",
   "icons/favicon-32x32.png",
-  "icons/favicon-16x16.png",
-  // Add any other images or files you want cached
+  "icons/favicon-16x16.png"
 ];
 
-// Install Service Worker
 self.addEventListener("install", (event) => {
-  console.log("[Service Worker] Installing...");
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("[Service Worker] Caching all: app shell and content");
       return cache.addAll(CACHE_ASSETS);
     })
   );
 });
 
-// Activate Service Worker
 self.addEventListener("activate", (event) => {
-  console.log("[Service Worker] Activating...");
-  // Clean up old caches
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
       );
     })
   );
 });
 
-// Fetch Handler
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // Return the cached response if found, else fetch from network
       return response || fetch(event.request);
     })
   );
